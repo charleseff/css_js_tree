965f892fa2537b8304b84014652acb30b5a182aa
Charles Finkel <charles.finkel@gmail.com>
2010-10-04 02:01:49 -0400

    refactor + testing per-request caching

--- lib/css_js_tree/core_ext.rb
+++ lib/css_js_tree/core_ext.rb
@@ -9,37 +9,48 @@ class ActionView::Partials::PartialRenderer
     if not @css_js_tree_request_cache.has_key? path_key
       @css_js_tree_request_cache[path_key] = nil
 
-      css_key = ('css_tree_partial_' + path).to_sym
-      js_key = ('js_tree_partial_' + path).to_sym
-
-      if not Rails.cache.exist?(css_key)
-        rel_css_location = CssJsTree.config[:css_tree_location] + @template.virtual_path + '.css'
-        if File.exists? File.join(Rails.root, 'public', 'stylesheets', rel_css_location)
-          @view.content_for :css_tree, @view.stylesheet_link_tag(rel_css_location)
-          Rails.cache.write(css_key, rel_css_location)
-        else
-          Rails.cache.write(css_key, nil)
-        end
-
-        rel_js_location = CssJsTree.config[:js_tree_location] + @template.virtual_path + '.js'
-        if File.exists? File.join(Rails.root, 'public', 'javascripts', rel_js_location)
-          @view.content_for :js_tree, @view.javascript_include_tag(rel_js_location)
-          Rails.cache.write(js_key, rel_js_location)
-        else
-          Rails.cache.write(js_key, nil)
-        end
-
-      else
-        @view.content_for :css_tree, Rails.cache.read(css_key) if Rails.cache.read(css_key)
-        @view.content_for :js_tree, Rails.cache.read(js_key) if Rails.cache.read(js_key)
-      end
-
+      render_css_include path
+      render_js_include path
     end
-
 
     render_partial_without_css_js_tree
   end
 
   alias_method_chain :render_partial, :css_js_tree
+
+  private
+  [:css, :js].each do |type|
+    case type
+      when :css
+        method_name = :render_css_include
+        key_prefix = 'css_tree_partial_'
+        tree_location = :css_tree_location
+        ext = '.css'
+        path_dir = 'stylesheets'
+        content_for = :css_tree
+        include_tag_method = :stylesheet_link_tag
+      when :js
+        method_name = :render_js_include
+        key_prefix = 'js_tree_partial_'
+        tree_location = :js_tree_location
+        ext = '.js'
+        path_dir = 'javascripts'
+        content_for = :js_tree
+        include_tag_method = :javascript_include_tag
+    end
+
+    define_method(method_name) do |path|
+      key = (key_prefix + path).to_sym
+
+      value = Rails.cache.fetch(key) do
+        rel_location = CssJsTree.config[tree_location] + @template.virtual_path + ext
+       ( File.exists? File.join(Rails.root, 'public', path_dir, rel_location) ) ?  rel_location : nil
+      end
+      @view.content_for content_for, @view.send(include_tag_method, value) if value
+
+    end
+  end
+
+
 
 end
--- test/css_js_tree_helper_test.rb
+++ test/css_js_tree_helper_test.rb
@@ -12,8 +12,8 @@ class CssJsTreeHelperTest < ActionView::TestCase
 
   test "css_tree returns the correct value" do
     File.stubs(:exists?).returns(false)
-    File.stubs(:exists?).with(File.join(Rails.root,'public','stylesheets', 'action_view.css')).returns(true)
-    File.stubs(:exists?).with(File.join(Rails.root,'public','stylesheets', 'action_view', 'test_case', 'test', 'foo.css')).returns(true)
+    File.expects(:exists?).with(File.join(Rails.root,'public','stylesheets', 'action_view.css')).returns(true)
+    File.expects(:exists?).with(File.join(Rails.root,'public','stylesheets', 'action_view', 'test_case', 'test', 'foo.css')).returns(true)
 
     assert_equal css_tree, '<link href="/stylesheets/action_view.css" media="screen" rel="stylesheet" type="text/css" />
 <link href="/stylesheets/action_view/test_case/test/foo.css" media="screen" rel="stylesheet" type="text/css" />'
@@ -22,8 +22,8 @@ class CssJsTreeHelperTest < ActionView::TestCase
 
   test "js_tree returns the correct values" do
     File.stubs(:exists?).returns(false)
-    File.stubs(:exists?).with(File.join(Rails.root,'public','javascripts', 'action_view.js')).returns(true)
-    File.stubs(:exists?).with(File.join(Rails.root,'public','javascripts', 'action_view', 'test_case', 'test', 'foo.js')).returns(true)
+    File.expects(:exists?).with(File.join(Rails.root,'public','javascripts', 'action_view.js')).returns(true)
+    File.expects(:exists?).with(File.join(Rails.root,'public','javascripts', 'action_view', 'test_case', 'test', 'foo.js')).returns(true)
 
     assert_equal js_tree, '<script src="/javascripts/action_view.js" type="text/javascript"></script>
 <script src="/javascripts/action_view/test_case/test/foo.js" type="text/javascript"></script>'
@@ -32,9 +32,10 @@ class CssJsTreeHelperTest < ActionView::TestCase
 
   test 'partial css and js get pulled in' do
     File.stubs(:exists?).returns(false)
-    File.stubs(:exists?).with(File.join(Rails.root,'public','stylesheets', '_test.css')).returns(true)
-    File.stubs(:exists?).with(File.join(Rails.root,'public','javascripts', '_test.js')).returns(true)
+    File.expects(:exists?).times(1).with(File.join(Rails.root,'public','stylesheets', '_test.css')).returns(true)
+    File.expects(:exists?).times(1).with(File.join(Rails.root,'public','javascripts', '_test.js')).returns(true)
 
+    render :partial => 'test'
     render :partial => 'test'
 
     assert_equal css_tree, '<link href="/stylesheets/_test.css" media="screen" rel="stylesheet" type="text/css" />'
