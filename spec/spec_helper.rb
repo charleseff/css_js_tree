164c5672c73112ada4bf74ffb9e2f33f4edc4be5
Charles Finkel <charles.finkel@gmail.com>
2010-10-21 01:29:46 -0400

    moving sample app, removing test/ dir

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,7 @@ $LOAD_PATH << File.join(File.dirname(__FILE__))
 require 'active_support'
 
 ENV['RAILS_ENV'] = 'test'
-ENV['RAILS_ROOT'] ||= File.dirname(__FILE__) + '/../test/fixtures/sample_rails3_app'
+ENV['RAILS_ROOT'] ||= File.dirname(__FILE__) + '/fixtures/sample_rails3_app'
 require File.expand_path(File.join(ENV['RAILS_ROOT'], 'config/environment.rb'))
 require 'rspec/rails'
 
