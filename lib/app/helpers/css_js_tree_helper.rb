042a4962662f59ed402e01914eea5f7c21e200b9
Charles Finkel <charles.finkel@gmail.com>
2010-10-21 16:21:53 -0400

    fixing to not use path methods again

--- lib/app/helpers/css_js_tree_helper.rb
+++ lib/app/helpers/css_js_tree_helper.rb
@@ -6,7 +6,7 @@ module CssJsTreeHelper
         method_name = :css_tree
         cache_prefix = 'css_'
         tree_location = :css_tree_location
-        path_method = :stylesheet_path
+        folder = 'stylesheets'
         include_tag_method = :stylesheet_link_tag
         content_for_sym = :css_tree
         ext = '.css'
@@ -14,7 +14,7 @@ module CssJsTreeHelper
         method_name = :js_tree
         cache_prefix = 'js_'
         tree_location = :js_tree_location
-        path_method = :javascript_path
+        folder = 'javascripts'
         include_tag_method = :javascript_include_tag
         content_for_sym = :js_tree
         ext = '.js'
@@ -30,7 +30,7 @@ module CssJsTreeHelper
         included = 1.upto(hierarchy.size).inject([]) do |array, i|
           rel_location = ((CssJsTree.config[tree_location] == '') ? '' : CssJsTree.config[tree_location] + '/') +
                   hierarchy[0, i].join('/') + ext
-          array << rel_location if File.exists? File.join(Rails.root, 'public', self.send(path_method,  rel_location) )
+                  array << rel_location if File.exists? File.join(Rails.root, 'public', folder, rel_location)
           array
         end
 
